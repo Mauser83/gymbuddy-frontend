53d7ce2ea5f60769abb1a1434981df55d92cd8af
Mauno Elo <mauser83@hotmail.com>
2025-08-29 12:28:17 +0300

    Update approveGymImage query and modal

--- src/features/cv/hooks/useApproveGymImage.ts
+++ src/features/cv/hooks/useApproveGymImage.ts
@@ -2,6 +2,25 @@ import {useMutation} from '@apollo/client';
 import {APPROVE_GYM_IMAGE} from '../graphql/candidateImages.graphql';
 
 export function useApproveGymImage() {
-  const [mutate, result] = useMutation(APPROVE_GYM_IMAGE);
+  const [mutate, result] = useMutation(APPROVE_GYM_IMAGE, {
+    update(cache, {data}) {
+      const gi = data?.approveGymImage?.gymImage;
+      if (!gi) return;
+      cache.modify({
+        id: cache.identify({__typename: 'GymEquipmentImage', id: gi.id}),
+        fields: {
+          status() {
+            return gi.status;
+          },
+          approvedAt() {
+            return gi.approvedAt;
+          },
+          approvedBy() {
+            return gi.approvedBy;
+          },
+        },
+      });
+    },
+  });
   return {mutate, ...result};
 }
